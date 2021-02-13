b87fef96ccd4dec2dbbd7b392916286d113eee4d
Сергей <kaluzhkikhsn@etf-int.ru>
2021-02-13 17:17:33 +0300

    start

---
+++ bantic_bev/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for bantic_bev project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bantic_bev.settings')
+
+application = get_wsgi_application()
